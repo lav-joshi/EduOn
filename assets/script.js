dc69dd12de6030f2fde32dabf0d6e22f77b16246
mightymoin <lcs2019006@iiitl.ac.in>
2020-09-05 21:27:48 +0530

    superUser structuring,chatbox style and stdentDiscuss

--- assets/script.js
+++ assets/script.js
@@ -2,20 +2,8 @@ var plant = document.querySelectorAll('#plant path');
 plant.forEach((path) => {
   path.style.fill = '#62760c';
 });
-// var navbar = document.querySelector('nav');
-// var sticky = navbar.offsetTop;
-// var logo = document.querySelectorAll('.nav-links div');
-// window.addEventListener('scroll', () => {
-//   if (window.pageYOffset >= sticky) {
-//     navbar.classList.add('sticky');
-//     logo[0].classList.add('logo-nav');
-//     logo[1].classList.add('logo-nav');
-//   } else {
-//     navbar.classList.remove('sticky');
-//     logo[0].classList.remove('logo-nav');
-//     logo[1].classList.remove('logo-nav');
-//   }
-// });
+var chatT = document.querySelectorAll('.T-chat');
+     M.Modal.init(chatT, {});
 document.addEventListener('DOMContentLoaded', function () {
   var datepicker = document.querySelectorAll('.datepicker');
   var timepicker = document.querySelectorAll('.timepicker');
