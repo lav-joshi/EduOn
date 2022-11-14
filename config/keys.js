90c1c06c9936c325ecfd82db9ad3b697430c77d6
Lav Joshi <lcs2019022@iiitl.ac.in>
2022-11-15 02:04:10 +0530

    API

--- config/keys.js
+++ config/keys.js
@@ -1,10 +1,10 @@
 module.exports = {
-    clientID: "428185779294-4bue4hiho7mqp2866jqs9vnugv7uskan.apps.googleusercontent.com",
-    clientSecret: "zCMmVUg_jPUZfztsJMxPVZ-m",
-    mongoURI: "mongodb+srv://lav_joshi:lav@joshi1056@cluster0.c4rc4.mongodb.net/eduthon?retryWrites=true&w=majority",
-    sessionSecret: "abc2"
-  };
-  
+  clientID: "428185779294-4bue4hiho7mqp2866jqs9vnugv7uskan.apps.googleusercontent.com",
+  clientSecret: "zCMmVUg_jPUZfztsJMxPVZ-m",
+  mongoURI: "mongodb+srv://lav_joshi:lav@joshi1056@cluster0.c4rc4.mongodb.net/eduthon?retryWrites=true&w=majority",
+  sessionSecret: "abc2"
+};
+
 // Super User Details :- 
 // Email Id :- eduon.portal@gmail.com
 // Password :- lav1234@ 
