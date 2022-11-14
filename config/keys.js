6786082981089134481fc690e5bef6bc5f2146e8
Lav Joshi <lcs2019022@iiitl.ac.in>
2022-11-15 02:28:55 +0530

    error

--- config/keys.js
+++ config/keys.js
@@ -1,7 +1,7 @@
 module.exports = {
   clientID: "428185779294-4bue4hiho7mqp2866jqs9vnugv7uskan.apps.googleusercontent.com",
   clientSecret: "zCMmVUg_jPUZfztsJMxPVZ-m",
-  mongoURI: "mongodb+srv://lav_joshi:lav@joshi1056@cluster0.c4rc4.mongodb.net/eduthon?retryWrites=true&w=majority",
+  mongoURI: "mongodb+srv://lav_joshi:lav@cluster0.c4rc4.mongodb.net/eduthon?retryWrites=true&w=majority",
   sessionSecret: "abc2"
 };
 
